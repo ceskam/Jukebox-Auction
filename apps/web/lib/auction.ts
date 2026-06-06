025cd441579fb9e12428af2e709c1a567f448913
Chace  Eskam <chaceaeskam@gmail.com>
2026-06-05 17:06:49 -0700

    Update auction.ts

--- apps/web/lib/auction.ts
+++ apps/web/lib/auction.ts
@@ -1,1 +1,13 @@
-export {};
+export interface Auction {
+  id: string;
+  highestBid: number;
+  winner: string | null;
+  endsAt: number;
+}
+
+export const currentAuction: Auction = {
+  id: "auction-1",
+  highestBid: 0,
+  winner: null,
+  endsAt: Date.now() + 15 * 60 * 1000,
+};
