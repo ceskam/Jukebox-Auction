b46b99967d57225a50d0ae5b3b4c635d68099cc5
Chace  Eskam <chaceaeskam@gmail.com>
2026-06-09 15:08:04 -0700

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,5 @@
 import BidButton from "../BidButton";
+import WalletConnect from "../WalletConnect";
 
 async function getAuction() {
   const res = await fetch("http://localhost:3000/api/auction", {
@@ -14,6 +15,8 @@ export default async function HomePage() {
   return (
     <main style={{ padding: "2rem" }}>
       <h1>Jukebox Auction</h1>
+
+      <WalletConnect />
 
       <p>Current Auction: {auction.id}</p>
 
